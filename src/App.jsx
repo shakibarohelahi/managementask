d76ce7e881b1f663534787526dd6ddf65e40b3ab
unknown <rohelahi.shakiba.pania@gmail.com>
2025-07-30 16:56:41 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import NewProject from './components/NewProject';
+import ProjectsSidebar from './components/ProjectsSidebar';
+
+import NoProjectSelected from "./components/NoProjectSelected";
+
+
+function App() {
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar />
+      <NoProjectSelected/>
+    </main>
+  );
+}
+
+export default App;
